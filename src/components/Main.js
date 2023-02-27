2602d5085f4a85e990d0acf9b654fba589272ab7
Ferdinand <bokallitechnology@gmail.com>
2023-02-27 08:45:35 +0100

    Add a count useState

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,15 +4,18 @@ import getCards from './getCards';
 
 export default function Main() {
     const [cards, setCards] = useState(() => getCards());
+    const [count, setCount] = useState(0);
 
     function setRandomCards() {
         const newCards = getCards();
         setCards(newCards);
+        setCount((prevCount) => prevCount.count + 1);
     }
 
     return (
         <div className="main">
             <div className="cards">
+                <>{count}</>
                 {cards
                     ? cards.map((card) => (
                           <Card
